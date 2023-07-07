5deeb13d0cd55e64e7bc05b978fc7bd9ed5cfae9
Damir <damir24may@mail.ru>
2023-07-08 02:55:23 +0300

    theme toggler

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "components/Container";
+import { Header } from "components/TheHeader";
+
+function App() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/TheHeader/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Header.module.scss";
+import { ThemeSwitcher } from "components/ThemeSwitcher";
+
+export const Header = () => (
+  <div className={styles.header}>
+    <div className={styles.logo}>devfinder</div>
+    <ThemeSwitcher />
+  </div>
+);
