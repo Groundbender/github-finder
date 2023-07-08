c0d7ee4ed17d02fb1716f83109ea270243225f9f
Damir <damir24may@mail.ru>
2023-07-08 19:39:01 +0300

    added searchForm button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Container } from "components/Container";
+import { Search } from "components/Search";
 import { Header } from "components/TheHeader";
 
 function App() {
   return (
     <Container>
       <Header />
+      <Search hasError onSubmit={() => {}} />
     </Container>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Button.module.scss";
+
+interface ButtonProps {
+  children: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button = ({ children, onClick }: ButtonProps) => (
+  <button onClick={onClick} className={styles.button}>
+    {children}
+  </button>
+);
