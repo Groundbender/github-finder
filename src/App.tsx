80b8da32abe30e741c7ccad9faa7538957e3ad97
Damir <damir24may@mail.ru>
2023-07-08 20:11:01 +0300

    added card stat components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Container } from "components/Container";
 import { Search } from "components/Search";
 import { Header } from "components/TheHeader";
+import { UserCard } from "components/UserCard";
+import { defaultUser } from "mock";
 
 function App() {
   return (
     <Container>
       <Header />
       <Search hasError onSubmit={() => {}} />
+      <UserCard {...defaultUser} />
     </Container>
   );
 }
---
+++ src/mock/index.ts
@@ -0,0 +1,18 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "Groundbender",
+  id: 111544215,
+  avatar: "https://avatars.githubusercontent.com/u/111544215?v=4",
+
+  name: "Дамир Насыбуллин",
+  company: null,
+  blog: "",
+  location: "Kazan",
+  bio: "frontend-developer 🧑‍💻\r\n",
+  twitter: null,
+  repos: 52,
+  followers: 0,
+  following: 0,
+  created: "2022-08-18T23:49:27Z",
+};
